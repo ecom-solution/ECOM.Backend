491c46e1f730462a6ac0486c5fed49d4ccfd749f
Tin Tran <gg.tintran@gmail.com>
2025-04-14 10:45:17 +0700

    remove

--- ECOM.Presentation.Workers.EmailConsumer/Program.cs
+++
@@ -1,7 +0,0 @@
-using ECOM.Presentation.Workers.EmailConsumer;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
--- ECOM.Presentation.Workers.NotifyConsumer/Program.cs
+++
@@ -1,7 +0,0 @@
-using ECOM.Presentation.Workers.NotifyConsumer;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
